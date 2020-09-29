ad66e01846632200a0047f56b65152dfa7776bb7
Mtsimmer <mtsimmer@gmail.com>
2020-09-30 01:27:19 +0300

    Parsing and interaction with Telegram left

--- main.py
+++ main.py
@@ -1,18 +1,49 @@
+"""General Notes
+
+
+Regarding the resource_ID
+registerd car numbers: 053cea08-09bc-40ec-8f7a-156f0677aff3
+diregisterd cars: 851ecab1-0622-4dbe-a6c7-f950cf82abf9 
+self import: 03adc637-b6fe-402b-9937-7c3d3afc9140
+MobileEye priceOff: 83bfb278-7be1-4dab-ae2d-40125a923da1
+
+
+##Maybe Usefull for further implamintation
+licensed resellers: eb74ad8c-ffcd-43bb-949c-2244fc8a8651
+statistics regarding models on the market: 5e87a7a1-2f6f-41c1-8aec-7216d52a6cf6 
+Recalls: 
+
+"""
+
+
 import requests
 import json
 
-URLS = ["https://data.gov.il/api/3/action/datastore_search?resource_id=053cea08-09bc-40ec-8f7a-156f0677aff3"]
+RESOURCE_IDS = ["053cea08-09bc-40ec-8f7a-156f0677aff3","851ecab1-0622-4dbe-a6c7-f950cf82abf9","03adc637-b6fe-402b-9937-7c3d3afc9140","83bfb278-7be1-4dab-ae2d-40125a923da1"]
+GENERIC_URL = "https://data.gov.il/api/3/action/datastore_search?resource_id="
 GENERIC_DATA = {'resource_id':None, #Contained in the url, depends on what API we are requesting
         'limit':1,  #limits the amount of answers
         'q':None} #The Query (Pretty much what the user provides)
 REQUEST_TYPE = 'POST'
 
 def main():
-    #will be improved to run on multiple apis
-    GENERIC_DATA['resource_id'] = '053cea08-09bc-40ec-8f7a-156f0677aff3'
-    GENERIC_DATA['q'] = '6477751'
-    reply = requests.request(REQUEST_TYPE,url=URLS[0],data=GENERIC_DATA)
-    print(reply.text)
+    print(query_apis("9489419")[0]['success']) #Simple Check weather the first api was good
+    print(query_apis("9489419")[1]['success'])
+    print(query_apis("9489419")[2]['success'])
+    print(query_apis("9489419")[3]['success'])
+
+
+def query_apis(q):
+    replies = [] #list of the returned jsons (a list of dicts)
+    for resource_id in RESOURCE_IDS:
+        url = GENERIC_URL + resource_id
+        data = GENERIC_DATA
+        data['resource_id'] = resource_id
+        data['q'] = q
+        raw_reply = requests.request(REQUEST_TYPE,url,data=data)
+        replies.append(json.loads(raw_reply.text))
+    return replies
+
 
 if __name__ == '__main__':
     main()
